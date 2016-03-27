A tentative fix in the numerical issue

src/fldb.py
@@ -14,7 +14,8 @@ def test_save_a():
     t0 = time.time()
     mat = materials.iso_metal_vm()
     #bnd = materials.prop_loading_short()
-    bnd = materials.prop_loading_long()
+    #bnd = materials.prop_loading_long()
+    bnd = materials.prop_loading_refine()
     pickle_fn = save_a(mat, bnd)
     print 'Elapsed time:', progress_bar.convert_sec_to_string(
         time.time() - t0)
@@ -80,7 +81,7 @@ def load_a(fn=None,hash_code=None):
     return data_collection_FLDA
 
 
-def main(f0=0.999,psi0=0):
+def main(f0=0.990,psi0=0):
     """
     Arguments
     ---------
@@ -101,8 +102,8 @@ def main(f0=0.999,psi0=0):
     mat.func_sr as a function of ebar dot
     mat.func_yd as a function of cauchy stress
     """
-    # bnd = materials.prop_loading_long()
-    bnd = materials.prop_loading_refine()
+    bnd = materials.prop_loading_long()
+    #bnd = materials.prop_loading_refine()
 
     ## Save FLDa
     FLDA_pck_name = save_a(mat, bnd)
@@ -117,22 +118,15 @@ def main(f0=0.999,psi0=0):
     dbar    = bnd.dbar
     ebar_mx = bnd.ebar_mx
 
-    ## Marciniak-Kuczynski parameters
-    n  = [1.,0.,0.];   t  = [0.,1.,0.]
-    f  = f0; psi = psi0
-
-    rot_mat = rot(psi)
-    n = lib.rot_vec(rot_mat,n)
-    t = lib.rot_vec(rot_mat,t)
-
     fmt='%7i %7.4f %7.4f %7.1e %7.4f %7.4f %7.1f'+\
         ' %7.1f %7.4f %7.4f %7.2f'
-    print ('%7s '*11)%(
+    head= ('%7s '*11)%(
         'stp','x0','f','df',
         'Eeq_A','Eeq_B','Seq_A','Seq_B',
         'EdeqA','EdeqB','ratio')
 
-    mx_dp = 5000
+    mx_dp = len(data_collection_FLDA[0][0])*5
+
     sig_A = np.zeros((bnd.nprob,6,mx_dp))*np.nan
     eps_A = np.zeros((bnd.nprob,6,mx_dp))*np.nan
     edt_A = np.zeros((bnd.nprob,  mx_dp))*np.nan
@@ -143,10 +137,17 @@ def main(f0=0.999,psi0=0):
     edt_B = np.zeros((bnd.nprob  ,mx_dp))*np.nan
     Seq_B = np.zeros((bnd.nprob  ,mx_dp))*np.nan
     Eeq_B = np.zeros((bnd.nprob  ,mx_dp))*np.nan
-
     times = np.zeros((bnd.nprob))*np.nan
 
     for i in xrange(bnd.nprob):
+        ## Marciniak-Kuczynski parameters
+        n  = [1.,0.,0.];   t  = [0.,1.,0.]
+        f  = f0; psi = psi0
+
+        rot_mat = rot(psi)
+        n = lib.rot_vec(rot_mat,n)
+        t = lib.rot_vec(rot_mat,t)
+
         eps_b_eq = 0.
         TT0 = time.time()
         ## Load from pickles.
@@ -154,7 +155,6 @@ def main(f0=0.999,psi0=0):
             = data_collection_FLDA[i]
 
         t  =  0.
-        x0 =  1e-5
         eps6_b=np.zeros((6,))
         # print 'nit, f1, f2, jac, x0, objf(x0)'
         plt.ioff()
@@ -176,31 +176,50 @@ def main(f0=0.999,psi0=0):
             sig6_b_grv  = c2s6(sig33_b_grv)
 
             sr33_b_grv      = np.zeros((3,3))
-            sr33_b_grv[0,0] =-1e-3 ## unknown
+            sr33_b_grv[0,0] = sr33_a_grv[0,0]
             sr33_b_grv[1,1] = sr33_a_grv[1,1]
-            sr33_b_grv[2,2] = 1e-3 ## unknown but linked with sr33_b_grv[0,0]
+            sr33_b_grv[2,2] = sr33_a_grv[2,2]
             sr33_b_grv[0,1] = sr33_a_grv[0,1]
             sr33_b_grv[1,0] = sr33_a_grv[1,0]
             sr6_b_grv       = c2s6(sr33_b_grv)
 
-            # print 'sr12_b_grv:',sr33_b_grv[0,1],sr33_b_grv[1,0]
-            # print 'sr12_a_grv:',sr33_a_grv[0,1],sr33_a_grv[1,0]
-            # return
+            if j==0:
+                x0 = 1e-4 #sr33_a_grv[0,0]
+                print 'x0:', x0
 
             ## Find e11_dot
             objf = modules.find_e11_dot(
                 sig6_b_grv,mat.func_yd,sr6_b_grv,
                 eps_b_eq,dt,mat.func_sr,mat.func_hd)
 
-            if np.mod(j,20)==0:
-                print 'Ea_eq :', '%6.4f'%eps_a_eq,
-                print 'Eb_eq :', '%6.4f'%eps_b_eq,
-                print 'sbar_a:', '%6.1f'%sbar_a
+            if np.mod(j,30)==0:
+                print head
+
+            if np.mod(j,100)==0 and False:
+                print 'Ea_eq     ',
+                print 'Eb_eq     ',
+                print 'sbar_a    ',
+                print 'siga_11   ',
+                print 'siga_22   ',
+                print 'sr11_a_grv',
+                print 'sr22_a_grv',
+                print 'sr33_a_grv',
+                print 'sr12_a_grv'
+
+                print '%11.4f'%eps_a_eq,
+                print '%11.4f'%eps_b_eq,
+                print '%11.1f'%sbar_a,
+                print '%11.1f'%sig6_a[0],
+                print '%11.1f'%sig6_a[1],
+                print '%11.1e'%sr33_a_grv[0,0],
+                print '%11.1e'%sr33_a_grv[1,1],
+                print '%11.1e'%sr33_a_grv[2,2],
+                print '%11.1e'%sr33_a_grv[0,1]
 
             ## N/R
-            irepeat = True; nit = 0; nit_mx = 100
-            err_tol = 1.5e-2
-            dx      = 1.e-7 ## debug?
+            irepeat = True; nit = 0; nit_mx = 1000
+            err_tol = 1e-1
+            dx      = 1.e-6 ## debug?
             verbose = False#True
             if verbose and np.mod(j,20)==0:
                 print '%4s %4s %4s %8s %8s %8s %8s'%(
@@ -208,24 +227,29 @@ def main(f0=0.999,psi0=0):
 
             while (irepeat):
                 f   = objf(x0)
-
-                if np.isnan(f):
-                    print 'x0:', x0
-                    raise IOError, 'objf(x0) led to nan'
-                if np.isnan(x0): raise IOError, 'x0 is nan'
-
-
+                # if np.isnan(f):
+                #     print 'x0:', x0
+                #     raise IOError, 'objf(x0) led to nan'
+                # if np.isnan(x0): raise IOError, 'x0 is nan'
 
                 try:
                     ## Jacobian as middle value
                     f1  = objf(x0-dx)
                     f2  = objf(x0+dx)
-                    jac = (f2-f1)/(dx*2)
-
+                    jac_new = (f2-f1)/(dx*2)
                 except:
                     ## Jacobian from forward direction
-                    f1  = objf(x0+dx)
-                    jac = (f1-f)/dx
+                    try:
+                        f1  = objf(x0+dx)
+                        jac_new = (f1-f)/dx
+                    except:
+                        f1  = objf(x0-dx)
+                        jac_new = -(f1-f)*dx
+
+                if jac_new == 0:
+                    pass
+                else:
+                    jac = jac_new
 
                 x0  = x0 - f/jac
 
@@ -276,7 +300,7 @@ def main(f0=0.999,psi0=0):
             Eeq_B[i,j]   = eps_b_eq
             edt_B[i,j]   = eps_b_eq_dot
 
-            times[i] = t
+            times[j] = t
 
             t = t + dt
 
@@ -290,10 +314,11 @@ def main(f0=0.999,psi0=0):
 
             ## check the thickness strain rate difference.
             ## write values.
+            print fmt%(j+1,x0,f,df,eps_a_eq,eps_b_eq,
+                       siga_eq,sigb_eq,eps_a_eq_dot,eps_b_eq_dot,
+                       ratio)
+
             if ratio>10:
-                print fmt%(j+1,x0,f,df,eps_a_eq,eps_b_eq,
-                           siga_eq,sigb_eq,eps_a_eq_dot,eps_b_eq_dot,
-                           ratio)
                 print 'Congratulations, you just failed'
                 x0 = 1e-4
                 break
@@ -311,9 +336,9 @@ def main(f0=0.999,psi0=0):
 
     for i in xrange(bnd.nprob):
         ax1.plot(eps_A[i,1,:],eps_A[i,0,:],'b-')
-        ax1.plot(eps_B[i,1,:],eps_B[i,0,:],'r-')
+        ax1.plot(eps_B[i,1,:],eps_B[i,0,:],'r-',zorder=-1)
         ax2.plot(sig_A[i,1,:],sig_A[i,0,:],'b-')
-        ax2.plot(sig_B[i,1,:],sig_B[i,0,:],'r-')
+        ax2.plot(sig_B[i,1,:],sig_B[i,0,:],'r-',zorder=-1)
         l, = ax3.plot(Eeq_A[i,:], Seq_A[i,:],ls='-')
         ax3.plot(Eeq_B[i,:], Seq_B[i,:],color=l.get_color(),ls='--')
 
@@ -344,3 +369,7 @@ def main(f0=0.999,psi0=0):
     plt.draw()
     plt.tight_layout()
     fig.savefig('FLD_results.pdf',bbox_inches='tight')
+
+
+if __name__=='__main__':
+    main()

src/materials.py
@@ -1,9 +1,13 @@
 """
 Materials database
 """
+
+import lib, flda
+reload(lib)
+reload(flda)
 from flda import *
-import lib
-## Material characteristics
+
+## Material Object
 class Mat_A:
     def __init__(self,name):
         self.name=name
@@ -43,8 +47,11 @@ class Mat_A:
             pass
 
         ys_ps, ys_pi = self.calc_ys()
+        for j in xrange(len(eps_sr)):
+            edt = eps_sr[j]
+            ax2.plot(ys_ps[0]*F(edt),ys_ps[1]*F(edt),
+                     label=r'$\mathrm{\dot{\bar{E}}^{eq}}=10^{%i}$'%np.log10(edt))
 
-        ax2.plot(ys_ps[0],ys_ps[1])
         ax2.set_xlabel(r'$\mathrm{\bar{\Sigma}^{11}}$',dict(fontsize=20))
         ax2.set_ylabel(r'$\mathrm{\bar{\Sigma}^{22}}$',dict(fontsize=20))
 
@@ -57,13 +64,14 @@ class Mat_A:
         fig.savefig('tension_test_%s.pdf'%self.name,bbox_inches='tight')
         plt.close(fig)
 
+## Material stocks
 def iso_metal_vm():
     """
     Isotropic Von Mises metal
     """
     my_iso_metal = Mat_A(name='iso_vm')
     my_iso_metal.assign_hd(ihd_opt=0,k=6.00e2,eps_0=4.23e-4,n=2.55e-1)
-    my_iso_metal.assign_sr(isr_opt=0,ed0=1e-3,m=0.02)
+    my_iso_metal.assign_sr(isr_opt=0,ed0=1e-3,m=0.10)
     my_iso_metal.assign_yd(iyd_opt=1) ## Von Mises yield surface
     return my_iso_metal
 
@@ -73,7 +81,7 @@ def iso_metal_hf8():
     """
     my_iso_metal = Mat_A(name='iso_hf')
     my_iso_metal.assign_hd(ihd_opt=0,k=6.00e2,eps_0=4.23e-4,n=2.55e-1)
-    my_iso_metal.assign_sr(isr_opt=0,ed0=1e-3,m=0.02)
+    my_iso_metal.assign_sr(isr_opt=0,ed0=1e-3,m=0.10)
     my_iso_metal.assign_yd(iyd_opt=2,a=8) ## Hosford exponent of 8
     return my_iso_metal
 
@@ -83,16 +91,18 @@ def aniso_metal_hill():
     """
     my_aniso_metal = Mat_A(name='aniso_hill')
     my_aniso_metal.assign_hd(ihd_opt=0,k=6.00e2,eps_0=4.23e-4,n=2.55e-1)
-    my_aniso_metal.assign_sr(isr_opt=0,ed0=1e-3,m=0.02)
+    my_aniso_metal.assign_sr(isr_opt=0,ed0=1e-3,m=0.10)
     ## Anisotropic Hill's quadratic tuned for the IF steel
     my_aniso_metal.assign_yd(iyd_opt=0,R0=2.1,R90=2.7)
     return my_aniso_metal
 
+## Visualize the constitutive behavior of the materials
 def tension_tests():
     iso_metal_vm().tension_test()
     iso_metal_hf8().tension_test()
     aniso_metal_hill().tension_test()
 
+## Boundary conditions
 class Bnd_A:
     def __init__(self,name,alpha0,alpha1,alphan,
                  beta,sr_eq,dbar,ebar_mx):
@@ -110,7 +120,7 @@ def prop_loading_short():
     """
     my_loading = Bnd_A(name='proportional_short',
                        alpha0=0,alpha1=1,alphan=3,beta=0,
-                       sr_eq=1e-3,dbar=1e-4,ebar_mx=0.1)
+                       sr_eq=1e-3,dbar=1e-3,ebar_mx=0.1)
     return my_loading
 
 def prop_loading_long():
@@ -118,15 +128,24 @@ def prop_loading_long():
     long proportional loading
     """
     my_loading = Bnd_A(name='proportional_long',
-                       alpha0=0,alpha1=1,alphan=3,beta=0,
-                       sr_eq=1e-3,dbar=1e-3,ebar_mx=1.4)
+                       alpha0=0,alpha1=1,alphan=15,beta=0,
+                       sr_eq=1e-3,dbar=5e-4,ebar_mx=1.5)
     return my_loading
 
 def prop_loading_refine():
     """
-    long proportional loading
+    long proportional loading with a refined step size
     """
     my_loading = Bnd_A(name='proportional_refine',
-                       alpha0=0,alpha1=1,alphan=3,beta=0,
-                       sr_eq=1e-3,dbar=1e-3,ebar_mx=1.0)
+                       alpha0=0,alpha1=1,alphan=5,beta=0,
+                       sr_eq=1e-3,dbar=1e-4,ebar_mx=1.0)
+    return my_loading
+
+def prop_loading_debug():
+    """
+    short proportional loading
+    """
+    my_loading = Bnd_A(name='proportional_short',
+                       alpha0=0.1,alpha1=1,alphan=1,beta=0,
+                       sr_eq=1e-3,dbar=1e-3,ebar_mx=0.10)
     return my_loading

src/modules.py
@@ -38,11 +38,28 @@ def find_e11_dot(
     func_F     (characterized strain rate function)
     func_G     (characterized strain hardening function)
     """
+
+    """
+    1.
+    sig_b is the stress that B region should have
+    at the current incremental step in order to meet
+    the force equilibrium condition
+
+    2.
+    deps_b should be the incremental strain
+    that B region should have at the current incremental
+    step in order to meet the compatibility condition
+    But only deps_b_11 is unknown and should be determined.
+    """
+
     sigb_eq = func_yld(sig_b)
-    deps_b  = deps_b_ref.copy()
+
+    # print 'sigb_eq:', sigb_eq
+    # raise IOError
 
     ## objective function to minimize
     def objf(e11dot):
+        deps_b  = deps_b_ref[::]
         deps_b[0] = e11dot
         deps_b[2] = -deps_b[0]-deps_b[1] ## incompressibility
         # wrate = np.dot(sig_b,deps_b) ## equivalent work rate
@@ -51,7 +68,24 @@ def find_e11_dot(
             wrate = wrate+sig_b[i]  *deps_b[i]
             wrate = wrate+sig_b[i+3]*deps_b[i+3]*2.
 
+        if wrate<0:
+            print 'sigb'
+            for j in xrange(6): print '%7.1f '%sig_b[j],
+            print
+            print 'deps_b'
+            for j in xrange(6): print '%7.1e '%deps_b[j],
+            print
+            #raise IOError,'wrate should be positive'
+            print 'wrate should be positive'
+
+        if eps_b_old<0:
+            raise IOError, 'epsb_old should be positive'
+
         eps_b_dot_eq_tilde = wrate / sigb_eq
+        # if eps_b_dot_eq_tilde<0:
+        #     eps_b_dot_eq_tilde=0.
+        #     eps_b_dot_eq_tilde=1e-10 ## very low value
+
         eps_b_tilde = eps_b_old + eps_b_dot_eq_tilde * dt
         F = func_F(eps_b_dot_eq_tilde)
         G = func_G(eps_b_tilde)