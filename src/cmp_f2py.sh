3071396526dff91388e837e70307dad0116624c8
Youngung Jeong <youngung.jeong@gmail.com>
2016-06-03 15:46:53 -0400

    yld2000 compile routine is added

--- src/cmp_f2py.sh
+++ src/cmp_f2py.sh
@@ -5,5 +5,15 @@ f2py -c -m for_lib for.f
 # compile func_fld_cy.pyx
 python cmp_cython.py build_ext --inplace
 
+
+
+## link yld2000
+gfortran -shared -fPIC -o yld2000_lib.so yld2000_lib.f
+#f2py --opt='-O3' -c -m yld2000 yld2000.f yld2000_lib.so
+#f2py  -c -m yld2000 yld2000.f yld2000_lib.so
+
+
+
+
 ## plotting vm
 python vm_check.py
