070404a22055d7fb5347c95f8b508a198d7a87b7
Youngung Jeong <youngung.jeong@gmail.com>
2016-06-13 10:47:56 -0400

    3d Yield surface is plotted in s11,s22,s12 space

--- mk/tests/mechtests.py
+++ mk/tests/mechtests.py
@@ -10,6 +10,7 @@ Various mechanical tests...
 """
 import numpy as np
 from mk.library.lib import rot_6d
+import mk.yieldFunction.yf2
 
 def inplaneTension(fYLD,iopt=1,**kwargs):
     """
@@ -96,3 +97,107 @@ def locus_shear(func,nth=100):
         X.append(ys[0])
         Y.append(ys[5])
     return np.array(X),np.array(Y)
+
+def defineObjf(th,yfunc,s12):
+    def obj(rad):
+        s11,s22=rad*np.cos(th),rad*np.sin(th)
+        s=[s11,s22,0,0,0,s12]
+        snew,phi,dphi,d2phi=yfunc(s)
+        snew = snew/phi
+        return abs(snew[5] - s12)
+    return obj
+
+def surface(func):
+    """"
+    3D surface of (s11,s22,s12)
+    Return (S11,S22) contours with a fixed level of s12.
+
+    Arguments
+    --------
+    func : yield fuction
+    """
+    import time
+    from scipy.optimize import minimize
+
+    t0=time.time()
+    ## find s12 yield stress
+    snew, phi, dphi, d2phi = func([0,0,0,0,0,1.])
+    y_s12 = snew[-1]
+
+    nlev_s12 = 20
+    s12Cnts = np.linspace(0,y_s12, nlev_s12)[:-1] ## exlude the last level
+
+    nths = 100
+    ths = np.linspace(-np.pi,np.pi,100) ## azimuth
+    yieldSurface = np.zeros((nlev_s12,nths,3)) ## altitude
+
+    rad = 1.
+    for iz in xrange(nlev_s12-1):
+        s12 = s12Cnts[iz] ## level, s12 should be met by this value.
+        for i in xrange(len(ths)):
+            objf = defineObjf(ths[i],func,s12Cnts[iz])
+            res = minimize(
+                fun=objf,x0=rad,
+                tol=1e-20,method='BFGS',jac=False)
+            rad = res.x
+            s11,s22 = rad*np.cos(ths[i]), rad*np.sin(ths[i])
+            snew,phi,dphi,d2phi = func([s11,s22,0,0,0,s12])
+            yieldSurface[iz,i,:]=snew[0],snew[1],snew[5]
+    print 'Elapsed time in mk.tests.mechtests.surface:', time.time()-t0
+    return yieldSurface, y_s12
+
+def testSurface(
+        yfunc = mk.yieldFunction.yf2.wrapHill48R([1.5,2.4,2.9])):
+    """
+    Calculate the 3D space yield surface in (s11,s22,s12)
+
+    Arguments
+    ---------
+    yfunc
+    """
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    import MP.lib.mpl_lib
+    import matplotlib as mpl
+
+    ## Test Hill48
+    # yfunc = mk.yieldFunction.yf2.wrapYLD(r=[1.5,2.4,2.9,1.],y=[1,1,1,1.3])
+    if type(yfunc).__name__=='int':
+        raise IOError
+
+    yieldSurface, y_s12 = surface(yfunc)
+    cmap = MP.lib.mpl_lib.color_map(mn=0,mx=y_s12,cmap='viridis')
+
+    fig1 = plt.figure()
+    ax1  = fig1.add_subplot(111)
+    fig2 = plt.figure(figsize=(6,5))
+    ax2  = fig2.add_subplot(111,projection='3d')
+    ax2.view_init(elev=25., azim=45.)
+
+    for i in xrange(len(yieldSurface)):
+        x=yieldSurface[i,:,0]
+        y=yieldSurface[i,:,1]
+        z=yieldSurface[i,:,2]
+
+        c = cmap(z[0])
+        ax1.plot(x,y,color=c)
+        ax2.plot(x,y,z,color=c)
+        pass
+
+    ax1.set_aspect('equal'); ax2.set_aspect('equal')
+    for ax in [ax1,ax2]:
+        ax.set_xlabel(r'$s_{11}$')
+        ax.set_ylabel(r'$s_{22}$')
+    ax2.set_zlabel(r'$s_{12}$')
+    ax2.set_xticks([-1,0,1])
+    ax2.set_yticks([-1,0,1])
+    ax2.set_zticks([0,0.5,1])
+
+    fig1.tight_layout(); fig2.tight_layout()
+    fig1.savefig('yieldsurface_2d.pdf',bbox_to_inches='tight')
+    fig2.savefig('yieldsurface_3d.pdf',bbox_to_inches='tight')
+
+    return yieldSurface, y_s12
+
+if __name__=='__main__':
+    testSurface()
