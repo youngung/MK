68910cde81e6c50499270aa06a03847fd0954ca4
Youngung Jeong <youngung.jeong@gmail.com>
2017-05-29 17:59:33 +0900

    Fixed setup_for.sh

--- setup_for.sh
+++ setup_for.sh
@@ -2,4 +2,4 @@
 ## setup the fortran codes for yield functions
 
 ## stand-alone executable
-gfortran fortran/yld2000.f fortran/yld2000_lib.f -o yld2000_sa
+gfortran fortran/yld2000_main.f fortran/yld2000.f fortran/yld2000_lib.f -o yld2000_sa
